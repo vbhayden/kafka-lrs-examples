e05d5bac9b2add2004ea3cda38aa9aeb76e88ea8
TreyH <vbhayden3@gmail.com>
2018-12-18 12:59:08 -0500

    removing single folder

--- single-node/rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop the existing docker containers we made with Compose
-docker-compose stop
-
-# Rebuild the containers and detatch from this terminal
-docker-compose build
-docker-compose up -d
